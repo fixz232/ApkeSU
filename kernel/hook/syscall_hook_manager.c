0e5ec5d6fcfb9636cfbb27e929366ae570df9eda
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2026-07-17 08:47:37 +0800

    kernel: register tracepoint handler with minimum priority (#3582)
    This allows other tracepoint handler like perf/bpf to run before we
    corrupt the context

--- kernel/hook/syscall_hook_manager.c
+++ kernel/hook/syscall_hook_manager.c
@@ -156,7 +156,7 @@ void __init ksu_syscall_hook_manager_init(void)
         pr_err("hook_manager: not all syscall handlers were registered\n");
 
 #ifdef CONFIG_HAVE_SYSCALL_TRACEPOINTS
-    ret = register_trace_sys_enter(ksu_sys_enter_handler, NULL);
+    ret = register_trace_prio_sys_enter(ksu_sys_enter_handler, NULL, INT_MIN);
 #ifndef CONFIG_KRETPROBES
     ksu_mark_running_process_locked();
 #endif
